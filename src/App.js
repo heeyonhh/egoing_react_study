3539e27830395f294ef5d855d6ee0a9bff0b6ebe
heeyonhh <lightram23@gmail.com>
2023-07-22 20:46:46 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+//리액트는 사용자 정의 태그를 만드는 기술이다
+//사용자 정의 태그를 컴포넌트 : 함수
+//함수의 리턴값이 컴포넌트의 ui
+
+function Counter(props){
+  console.log(props);
+  
+  return <div>
+      <h1>Counter</h1>
+      <button>+</button> 0
+    </div>
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Counter title="불면증 카운터"></Counter>
     </div>
   );
 }
